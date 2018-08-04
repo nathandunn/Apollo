a2de706b7d36666387bc60c8b3780ee001b3303f
Nathan Dunn <ndunn@me.com>
2018-08-04 13:11:21 -0700

    bad start

---
+++ grails-app/controllers/org/bbop/apollo/GenomeFeatureController.groovy
@@ -0,0 +1,16 @@
+package org.bbop.apollo
+
+
+import org.restapidoc.annotation.RestApi
+
+/**
+ */
+@RestApi(name = "Genome Features Services", description = "Methods for viewing genome features")
+class GenomeFeatureController {
+
+
+    def showGene(){
+
+    }
+
+}
